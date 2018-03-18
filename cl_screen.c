20f00a25f314aa710774ffa86e42a3478b10ae96
meag <meag@acm.org>
2018-03-18 13:50:56 +0000

    FRAMEBUFFERS: Add support for screen-scaling... and then remove for now
    Old branch, aim was to get rid of having to toggle vid_usedesktopres, if
      it was enabled and the user chose a different resolution, it would just
      render to framebuffer and then upscale to native res.
    At the moment our fragment shaders aren't heavy enough for this to help,
      and without anti-aliasing on the final image it looks pretty terrible
      anyway.
    Don't want to complicate initial testing with this, so leaving out of the
      build for the moment (have to #define SUPPORT_FRAMEBUFFERS to include)

--- cl_screen.c
+++ cl_screen.c
@@ -2147,10 +2147,10 @@ void SCR_UpdateScreenPlayerView(int flags)
 
 	SCR_SetupCI();
 
+	GL_BeginRendering(&glx, &gly, &glwidth, &glheight);
+
 	if (V_PreRenderView()) {
 		R_SetupFrame();
-
-		GL_BeginRendering(&glx, &gly, &glwidth, &glheight);
 
 		R_RenderView();
 
