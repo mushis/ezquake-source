12b4820cd68a2ea77bec7165007b883c499c4fbc
meag <meag@acm.org>
2018-04-12 23:15:49 +0100

    CLASSIC: Fix gl_textureless when using VBO

--- sbar.c
+++ sbar.c
@@ -1162,7 +1162,7 @@ static void Sbar_DeathmatchOverlay(int start)
 {
 	int stats_basic, stats_team, stats_touches, stats_caps, playerstats[7];
 	int scoreboardsize, colors_thickness, statswidth, stats_xoffset = 0;
-	int i, d, k, top, bottom, x, y, xofs, total, p, skip = 10, fragsint;
+	int i, k, top, bottom, x, y, xofs, total, p, skip = 10, fragsint;
 	int rank_width, leftover, startx, tempx, mynum;
 	char num[12];
 	char myminutes[11];
